0e95858c79ef1135313a76aa5b9d40cb8e03df36
Gallaugher <john.gallaugher@bc.edu>
2017-02-06 21:19:34 -0500

    Exercise 3.1 from "Intro to Programming using Swift for iOS Development"

--- cupConvert/ViewController.swift
+++ cupConvert/ViewController.swift
@@ -10,6 +10,9 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var cupField: UITextField!
+    @IBOutlet weak var resultsLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -20,6 +23,35 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    func showAlert(title: String, message: String) {
+        let alert = UIAlertController(title: title, message: message, preferredStyle: .alert)
+        let alertAction = UIAlertAction(title: "OK", style: .default, handler: nil)
+        alert.addAction(alertAction)
+        present(alert, animated: true, completion: nil)
+    }
+    
+    @IBAction func convertPressed(_ sender: UIButton) {
+        
+        if let cups = Double(cupField.text! ) {
+            
+            let oz = cups * 8
+            let tbsp = cups * 16
+            let quarts = cups * 0.25
+            let pints = cups * 0.5
+            
+            let resultsText = "\(cups) cups = \(oz) ounces, \(tbsp) tablespoons, \(quarts) quarts, or \(pints) pints"
+            resultsLabel.text = resultsText
+        } else {
+            resultsLabel.text = ""
+            
+            let titleString = "Error:"
+            let messageString = "\(cupField.text!) is not a valid number"
 
+            showAlert(title: titleString, message: messageString)
+        }
+        
+    }
+    
+    
 }
 
